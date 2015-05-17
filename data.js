47aabe6135cdd6cee3ef58df72cc5f50775df0bf
Justin McConnell <boolean@gmail.com>
2015-05-17 14:37:56 -0700

    Instead of hardcoding, get file UI data from a javascript object.

---
+++ data.js
@@ -0,0 +1,22 @@
+var files = {
+  "benders": {
+    "name": "benders",
+    "files": {
+      "fire": {
+        "name": "fire",
+        "modified": "2 days ago",
+        "directory": true
+      },
+      "water": {
+        "name": "water",
+        "modified": "4 days ago",
+        "directory": true
+      },
+      "aang": {
+        "name": "aang",
+        "modified": "10 minutes ago",
+        "content": "Aang is the Avatar."
+      } 
+    }
+  }
+};
